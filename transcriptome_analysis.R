c2aba4cb4b4465e1527f84d1bc755ab83a991077
Christophe Desterke <cdesterke@users.noreply.github.com>
2020-10-22 21:05:30 +0200

    Update transcriptome_analysis.R

--- transcriptome_analysis.R
+++ transcriptome_analysis.R
@@ -1,1 +1,19 @@
+# expression boxplot stratified by groups for two markers
+library(ggpubr)
+ggboxplot(set, x = "SARSCOV2",
+          y = c("CALR","CANX"),
+          combine = TRUE,
+          color = "SARSCOV2", palette = c("black","red","green"),
+          ylab = "Expression", 
+          add = "jitter",                              
+          add.params = list(size = 2, jitter = 0.35)  
+          )
+
+
+# plot of correlation analysis of markers in transcriptome assay
+library(corrgram)
+
+corrgram(data, order=TRUE, lower.panel=panel.shade,
+  upper.panel=panel.pie,   #diag.panel=panel.minmax,
+  main="COVID19 lung immunoproteasome") 
 
